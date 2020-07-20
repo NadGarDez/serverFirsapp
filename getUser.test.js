38a18c15e0bd2ce9d724a5da8a09725f27aefe42
nade <pirastri@gmail.com>
2020-07-20 17:51:03 +0000

    cambio de ruta

---
+++ getUser.test.js
@@ -0,0 +1,14 @@
+modulo = require('./modulosPropios/getUser.js');
+var mysql = require('./mysqlMnager.js');
+
+test('probar consulta mysql', () => {
+	var consulta = "SELECT * FROM usuario WHERE id=4";
+	return mysql.consultar2(consulta).then(
+		data=>{
+			expect(data).not.toBeUndefined();
+			console.log(data);
+		}
+	)
+	
+	
+});
