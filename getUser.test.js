e3d3ba528da68b20c783856fbb5c39bb12dea489
Nad GarDez <iranad@hotmail.com>
2020-07-22 13:56:46 -0400

    abc

--- getUser.test.js
+++ getUser.test.js
@@ -1,6 +1,6 @@
 modulo = require('./modulosPropios/getUser.js');
 var mysql = require('./modulosPropios/mysqlMnager.js');
-ini = require('./modulosPropios/fetchManager.js');
+superFetch= require('./modulosPropios/fetchManager.js');
 
 test('probar consulta mysql', () => {
 	var consulta = "SELECT * FROM usuario WHERE id=4";
@@ -60,7 +60,7 @@ test('probar get user',
 
 		var data ='id=4';
     
-	    ini("http:\//167.71.173.198:3000/getUser",true,null,data,'post','json',
+	    superFetch.ini("http:\//167.71.173.198:3000/getUser",true,null,data,'post','json',
 	      (data)=>{
 	       ///root@167.71.173.198:3000/proyects/serverFirsapp
 	              
